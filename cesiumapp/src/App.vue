cbb3ca0921a0c4f143daaadd8e296824782cb62a
Theodora91131 <10556003@ntub.edu.tw>
2023-02-06 15:19:31 +0800

    feat:initial

---
+++ cesiumapp/src/App.vue
@@ -0,0 +1,28 @@
+<script setup>
+import {  Viewer } from "cesium";
+import { onMounted  } from "vue";
+onMounted(()=>{
+  const viewer = new Viewer("cesiumContainer");
+  const iframe = document.getElementsByClassName("cesium-infoBox-iframe")[0];
+  iframe.setAttribute(
+    "sandbox",
+    "allow-some-origin allow-scripts allow-popups allow-forms"
+  );
+  iframe.setAttribute("src","");
+
+});
+</script>
+
+<template>
+  <div id="cesiumContainer"></div>
+  
+</template>
+
+<style scoped>
+#cesiumContainer{
+  width:100%;
+  height:100%;
+  position:absolute;
+ 
+}
+</style>
